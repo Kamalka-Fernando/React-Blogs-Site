bbfc7e27a021065e8c0521af9c33728187f505c3
Kamalka Fernando <macbookpro@Fernando.local>
2022-02-11 19:22:48 +0530

    first try in react

---
+++ src/useFetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() =>{
+        setTimeout(() => {
+            fetch(url)
+            .then(res => {
+                if(!res.ok){
+                    throw Error("Could not find data for that resource");
+                }
+                return res.json()
+            })
+            .then(data =>{
+                console.log(data);
+                setData(data);
+                setIsPending(false);
+                setError(null);
+            }, 1000)
+            .catch(err =>{
+                setIsPending(false);
+                setError(err.message);
+            })
+        })
+    }, [url]);
+
+    return { data, isPending, error }
+}
+ 
+export default useFetch;
